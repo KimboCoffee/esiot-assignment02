65a6f89d336454e92eb670ff61d9597d2d8a244b
Kimi Osti <kimi.osti@studio.unibo.it>
2024-11-20 18:08:08 +0100

    Removed message dispatcher to collapse logic in message handler

--- project/pc/MessageHandler.py
+++ project/pc/MessageHandler.py
@@ -1,13 +1,30 @@
 import serial
 
+class Message:
+    def __init__(self, temp, level, exceptionType = 0):
+        self.temp = temp
+        self.level = level
+        self.exceptionType = exceptionType
+
+    def getTemp(self):
+        return self.temp
+    
+    def getLevel(self):
+        return self.level
+    
+    def getExceptionType(self):
+        return self.exceptionType
+
 class MessageHandler:
     def __init__(self):
         self.baudrate = 9600
         self.port = "COM4"
         self.device = serial.Serial(self.port, self.baudrate)
 
-    def send(self, msg):
-        self.device.write(msg)
+    def send(self, text):
+        self.device.write(text)
 
     def read(self):
-        return self.device.read()
+        #metodo che legge finché non trova "END", e poi restituisce un Message che contiene tutte le info
+        self.device.read()
+        return 0

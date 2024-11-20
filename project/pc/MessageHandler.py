4512c927b6b212c8f0671ccb72c9725a159569af
Kimi Osti <kimi.osti@studio.unibo.it>
2024-11-20 17:54:55 +0100

    Fixed device initialisation

--- project/pc/MessageHandler.py
+++ project/pc/MessageHandler.py
@@ -4,7 +4,7 @@ class MessageHandler:
     def __init__(self):
         self.baudrate = 9600
         self.port = "COM4"
-        self.device = serial.Serial(port, baudrate)
+        self.device = serial.Serial(self.port, self.baudrate)
 
     def send(self, msg):
         self.device.write(msg)

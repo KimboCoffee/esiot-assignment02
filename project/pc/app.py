1e5b8e2a3e5aaf309f6c160b2aa2ba367a7416f2
Kimi Osti <kimi.osti@studio.unibo.it>
2024-11-22 10:19:06 +0100

    Added basic implementation of the application's behavior

--- project/pc/app.py
+++ project/pc/app.py
@@ -1,2 +1,12 @@
-import MessageDispatcher
-import GUI
+import MessageHandler
+import View
+import time
+
+gui = View.GUI()
+i = 0
+gui.mainMenu(i, i+1)
+
+while True:
+    gui.update(i, i+1)
+    i = i+1
+    time.sleep(0.200)

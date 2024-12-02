e7fefe16cff66043aac9610aa74a11c85ffb6d9e
Kimi Osti <kimi.osti@studio.unibo.it>
2024-12-02 21:25:46 +0100

    Removed constant definitions, since they were duplicate from single tasks

--- project/arduino/smart_waste_disposal/src/main.cpp
+++ project/arduino/smart_waste_disposal/src/main.cpp
@@ -6,26 +6,6 @@
 #include "communication_task.h"
 #include "container_handler_task.h"
 
-/*analog pins*/
-#define TEMP_SENSOR A0
-#define LCD_SDA A4
-#define LCD_SCL A5
-
-/*digital pins*/
-#define GREEN_LED 12
-#define RED_LED 13
-#define OPEN_BUTTON 11
-#define CLOSE_BUTTON 10
-#define DOOR 9
-#define LEVEL_ECHO 8
-#define LEVEL_TRIG 7
-#define USER_DETECTOR 2
-
-/*LCD settings*/
-#define LCD_ADDRESS 0x27
-#define LCD_ROWS 4
-#define LCD_COLS 20
-
 SystemStateTracker *systemTracker;
 LevelMonitorTask *levelMonitor;
 TempMonitorTask *tempMonitor;
